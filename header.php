35c492112abf88b2e36dd7ab1e59a4508d698adc
Artem Solovev <artem.solovev.lse@gmail.com>
2016-06-14 20:29:23 +0300

    Text in mobile menu replaced to icon

--- header.php
+++ header.php
@@ -84,7 +84,7 @@
                 </div><!-- .site-branding -->
 
                 <nav id="site-navigation" class="main-navigation" role="navigation">
-                        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'artfolio' ); ?></button>
+                        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
 
                         <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 

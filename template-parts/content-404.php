c923b6d18474e563ca48190394482900f775f8c2
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-02 12:33:52 +0300

    Added homePage link to 404 section

--- template-parts/content-404.php
+++ template-parts/content-404.php
@@ -1,5 +1,6 @@
 <div class="error404Title">
     <h1>404</h1>
-    <p><?php _e( 'You seem to be lost. To find what you are looking for try a search', 'artfolio' ); ?></p>
+    <p><?php _e( 'You seem to be lost. To find what you are looking for try go home or try a search', 'artfolio' ); ?></p>
+    <a href="<?php echo get_home_url(); ?>"><i class="fa fa-home"></i></a>
     <?php get_search_form(); ?>
 </div>

b682e101155f3765c7f1231712795059ee4437fc
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-03 14:58:23 +0300

    Added config for GRUNT js

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        pot: {
+            options:{
+                text_domain: 'artfolio', //Your text domain. Produces my-text-domain.pot
+                dest: 'languages/', //directory to place the pot file
+                keywords: ['_e', '__'], //functions to look for
+            },
+            files:{
+                src:  [ '**/*.php' ], //Parse all php files
+                expand: true,
+            }
+        }
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-pot');
+
+    grunt.registerTask('default', ['pot']);
+    grunt.registerTask('test', ['']);
+};

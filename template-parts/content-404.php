79ee8c095e8c34f3796d334f4efe82764ad364a0
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-09 21:03:08 +0300

    Changed title text

--- template-parts/content-404.php
+++ template-parts/content-404.php
@@ -1,5 +1,5 @@
 <div class="error404Title">
-    <h1 class="statusCode">404</h1>
+    <h1 class="statusCode">Oops</h1>
     <p><?php _e( 'You seem to be lost. To find what you are looking for try go home or try a search', 'artfolio' ); ?></p>
     <a href="<?php echo get_home_url(); ?>"><i class="fa fa-home"></i></a>
     <?php get_search_form(); ?>

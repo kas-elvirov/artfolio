e73a6efc61283b89737801c3a5f00e08ebe25e41
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-10 10:51:40 +0300

    #21 Added copyright to Admin section

--- functions.php
+++ functions.php
@@ -11,117 +11,124 @@
 class artfolio_recent_posts extends WP_Widget {
 
     function __construct() {
-            parent::__construct(
-                // Widget ID
-                'artfolio_widget', 
+        parent::__construct(
+            // Widget ID
+            'artfolio_widget', 
 
-                // Name of the widget
-                __('Artfolio recent posts', 'artfolio'), 
+            // Name of the widget
+            __('Artfolio recent posts', 'artfolio'), 
 
-                // Description of the widget
-                array( 'description' => __( 'Custom widget for recent posts with image and description', 'artfolio' ), ) 
-            );
+            // Description of the widget
+            array( 'description' => __( 'Custom widget for recent posts with image and description', 'artfolio' ), ) 
+        );
     }
 
     // Open code for widget
     public function widget( $args, $instance ) {
-            $title = apply_filters( 'widget_title', $instance['title'] );
-            $number = apply_filters( 'widget_number', absint( $instance['number']  ));
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        $number = apply_filters( 'widget_number', absint( $instance['number']  ));
 
-            echo $args['before_widget'];
+        echo $args['before_widget'];
 
-            if ( !empty( $title ) ) {
-                echo $args['before_title'] . $title . $args['after_title'];
-            }
+        if ( !empty( $title ) ) {
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
 
-            $art_recent_posts = new WP_Query();
-            $art_recent_posts->query('showposts='.$number);
+        $art_recent_posts = new WP_Query();
+        $art_recent_posts->query('showposts='.$number);
 
-            while ( $art_recent_posts->have_posts() ) {
-                $art_recent_posts->the_post();
-    ?>
-    <ul>
-        <li>
+        while ( $art_recent_posts->have_posts() ) {
+            $art_recent_posts->the_post();
+?>
+<ul>
+    <li>
 
-            <?php if ( has_post_thumbnail() ) { ?>
-            <a href="<?php esc_url( the_permalink() ); ?>" class="arp-thumbnail">
-                <?php the_post_thumbnail( 'artfolio-recent-thumbnails' ); ?>
-            </a>
-            <?php } else {?>
-            <a href="<?php esc_url( the_permalink() ); ?>" class="arp-thumbnail imagePlugWrapper">
-                <i class="fa fa-picture-o imagePlug" aria-hidden="true"></i>
-            </a>
-            <?php }?>
-
-            <header class="arp-header"
-                    <h3>
-            <a href="<?php esc_url( the_permalink() ); ?>" class="arp-title"><?php esc_html( the_title() ); ?></a>
-        </h3>
-
-    <div class="arp-meta">
-        <p class="arp-time"><?php the_time( 'F jS, Y' ); ?></p>
-        <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="arp-author">
-            <?php esc_html( the_author() ); ?>
+        <?php if ( has_post_thumbnail() ) { ?>
+        <a href="<?php esc_url( the_permalink() ); ?>" class="arp-thumbnail">
+            <?php the_post_thumbnail( 'artfolio-recent-thumbnails' ); ?>
         </a>
-    </div> <!-- .arp-header-->
-    </header>
-    </li>
-    </ul>
-    <?php
-            }
-            echo $args['after_widget'];
+        <?php } else {?>
+        <a href="<?php esc_url( the_permalink() ); ?>" class="arp-thumbnail imagePlugWrapper">
+            <i class="fa fa-picture-o imagePlug" aria-hidden="true"></i>
+        </a>
+        <?php }?>
+
+        <header class="arp-header"
+                <h3>
+        <a href="<?php esc_url( the_permalink() ); ?>" class="arp-title"><?php esc_html( the_title() ); ?></a>
+    </h3>
+
+<div class="arp-meta">
+    <p class="arp-time"><?php the_time( 'F jS, Y' ); ?></p>
+    <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="arp-author">
+        <?php esc_html( the_author() ); ?>
+    </a>
+</div> <!-- .arp-header-->
+</header>
+</li>
+</ul>
+<?php
+        }
+        echo $args['after_widget'];
     }
 
     // Close code for widget
     public function form( $instance ) {
 
-            if ( isset( $instance[ 'number' ] ) ) {
-                $number = $instance[ 'number' ];
-            } else {
-                $number = 5;
-            }
+        if ( isset( $instance[ 'number' ] ) ) {
+            $number = $instance[ 'number' ];
+        } else {
+            $number = 5;
+        }
 
-            if ( isset( $instance[ 'title' ] ) ) {
-                $title = $instance[ 'title' ];
-            } else {
-                $title = __( 'Recent posts', 'artfolio' );
-            }
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        } else {
+            $title = __( 'Recent posts', 'artfolio' );
+        }
 
-            // For administration console
-    ?>
-    <p>
-        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'artfolio' ); ?></label> 
-        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-    </p>
+        // For administration console
+?>
+<p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'artfolio' ); ?></label> 
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
 
-    <p>
-        <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:', 'artfolio' ); ?></label>
-        <input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" />
-    </p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:', 'artfolio' ); ?></label>
+    <input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" />
+</p>
 
-    <?php
+<?php
     }
 
     // Widget update
     public function update( $new_instance, $old_instance ) {
 
-            $instance = array();
+        $instance = array();
 
-            $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 
-            $instance['number'] = ( ! empty( $new_instance['number'] ) ) ? strip_tags( $new_instance['number'] ) : 5;
+        $instance['number'] = ( ! empty( $new_instance['number'] ) ) ? strip_tags( $new_instance['number'] ) : 5;
 
-            return $instance;
+        return $instance;
     }
 
 } // end class recent_posts
 
 // Widget registration
 function artfolio_widget_recentposts() {
-        register_widget( 'artfolio_recent_posts' );
+    register_widget( 'artfolio_recent_posts' );
 }
 
 add_action( 'widgets_init', 'artfolio_widget_recentposts' );
+
+// Modify Admin Footer Text
+function modify_footer() {
+    echo 'Created by <a href="https://github.com/artem-solovev" target="_blank">Artem Solovev</a> in 2016. All rights reserved';
+}
+
+add_action( 'admin_footer_text', 'modify_footer' );
 
 
 
@@ -135,74 +142,74 @@ if ( ! function_exists( 'artfolio_setup' ) ) :
  */
 function artfolio_setup() {
 
-        // This theme styles the visual editor to resemble the theme style.
-        $font_url = 'https://fonts.googleapis.com/css?family=Merriweather:400,300,400italic,700,900,900italic|Open+Sans:400,400italic,700,700italic';
-        add_editor_style( array( 'inc/editor-style.css', str_replace( ',', '%2C', $font_url ) ) );
+    // This theme styles the visual editor to resemble the theme style.
+    $font_url = 'https://fonts.googleapis.com/css?family=Merriweather:400,300,400italic,700,900,900italic|Open+Sans:400,400italic,700,700italic';
+    add_editor_style( array( 'inc/editor-style.css', str_replace( ',', '%2C', $font_url ) ) );
 
-        /*
+    /*
        * Make theme available for translation.
        * Translations can be filed in the /languages/ directory.
        * If you're building a theme based on Artfolio, use a find and replace
        * to change 'artfolio' to the name of your theme in all the template files.
        */
-        load_theme_textdomain( 'artfolio', get_template_directory() . '/languages' );
+    load_theme_textdomain( 'artfolio', get_template_directory() . '/languages' );
 
-        // Add default posts and comments RSS feed links to head.
-        add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
 
-        /*
+    /*
        * Let WordPress manage the document title.
        * By adding theme support, we declare that this theme does not use a
        * hard-coded <title> tag in the document head, and expect WordPress to
        * provide it for us.
        */
-        add_theme_support( 'title-tag' );
+    add_theme_support( 'title-tag' );
 
-        /*
+    /*
        * Enable support for Post Thumbnails on posts and pages.
        *
        * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
        */
-        add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-thumbnails' );
 
-        add_image_size( 'artfolio-recent-thumbnails', 100, 100, true ); // Sets Recent Posts Thumbnails
-        add_image_size( 'large-thumbnails', 1280, 220, true ); // Sets Large post thumbnails for header in single post section
-        add_image_size( 'small-thumbnails', 780, 300, true ); // Sets Small post thumbnails for posts in index post section
+    add_image_size( 'artfolio-recent-thumbnails', 100, 100, true ); // Sets Recent Posts Thumbnails
+    add_image_size( 'large-thumbnails', 1280, 220, true ); // Sets Large post thumbnails for header in single post section
+    add_image_size( 'small-thumbnails', 780, 300, true ); // Sets Small post thumbnails for posts in index post section
 
-        // This theme uses wp_nav_menu() in one location.
-        register_nav_menus( array(
-            'primary' => esc_html__( 'Primary', 'artfolio' ),
-            'social' => __( 'Social Menu', 'artfolio'),
-            'landing' => __( 'Landing Menu', 'artfolio' ),
-        ) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => esc_html__( 'Primary', 'artfolio' ),
+        'social' => __( 'Social Menu', 'artfolio'),
+        'landing' => __( 'Landing Menu', 'artfolio' ),
+    ) );
 
-        /*
+    /*
        * Switch default core markup for search form, comment form, and comments
        * to output valid HTML5.
        */
-        add_theme_support( 'html5', array(
-            'search-form',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption',
-        ) );
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
 
-        /*
+    /*
        * Enable support for Post Formats.
        * See https://developer.wordpress.org/themes/functionality/post-formats/
        */
-        add_theme_support( 'post-formats', array( 'aside' ) );
+    add_theme_support( 'post-formats', array( 'aside' ) );
 
-        /*
+    /*
        * Enable support for Custom logo.
        * See http://codex.wordpress.org/Function_Reference/add_theme_support#Custom_Logo
        */
-        add_theme_support( 'custom-logo', array(
-            'height'      => 130,
-            'width'       => 130,
-            'flex-width' => false,
-        ) );
+    add_theme_support( 'custom-logo', array(
+        'height'      => 130,
+        'width'       => 130,
+        'flex-width' => false,
+    ) );
 }
 endif;
 
@@ -216,7 +223,7 @@ add_action( 'after_setup_theme', 'artfolio_setup' );
  * @global int $content_width
  */
 function artfolio_content_width() {
-        $GLOBALS['content_width'] = apply_filters( 'artfolio_content_width', 700 );
+    $GLOBALS['content_width'] = apply_filters( 'artfolio_content_width', 700 );
 }
 add_action( 'after_setup_theme', 'artfolio_content_width', 0 );
 
@@ -226,25 +233,25 @@ add_action( 'after_setup_theme', 'artfolio_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function artfolio_widgets_init() {
-        register_sidebar( array(
-            'name'          => esc_html__( 'Sidebar', 'artfolio' ),
-            'id'            => 'sidebar-1',
-            'description'   => esc_html__( 'Add widgets here.', 'artfolio' ),
-            'before_widget' => '<section id="%1$s" class="widget %2$s">',
-            'after_widget'  => '</section>',
-            'before_title'  => '<h2 class="widget-title">',
-            'after_title'   => '</h2>',
-        ) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'artfolio' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'artfolio' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 
-        register_sidebar( array(
-            'name'          => __( 'Footer Widgets', 'artfolio' ),
-            'description'   => __( 'Footer widgets area appears in the footer of the site.', 'artfolio' ),
-            'id'            => 'sidebar-2',
-            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-            'after_widget'  => '</aside>',
-            'before_title'  => '<h1 class="widget-title">',
-            'after_title'   => '</h1>',
-        ) );
+    register_sidebar( array(
+        'name'          => __( 'Footer Widgets', 'artfolio' ),
+        'description'   => __( 'Footer widgets area appears in the footer of the site.', 'artfolio' ),
+        'id'            => 'sidebar-2',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
 }
 add_action( 'widgets_init', 'artfolio_widgets_init' );
 
@@ -252,33 +259,33 @@ add_action( 'widgets_init', 'artfolio_widgets_init' );
  * Enqueue scripts and styles.
  */
 function artfolio_scripts() {
-        wp_enqueue_style( 'artfolio-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'artfolio-style', get_stylesheet_uri() );
 
-        if (is_page_template('template-pages/page-nosidebar.php')) {
-            wp_enqueue_style( 'artfolio-layout-style' , get_template_directory_uri() . '/layouts/nosidebar.css');
-        } else {
-            wp_enqueue_style( 'artfolio-layout-style' , get_template_directory_uri() . '/layouts/content-sidebar.css');
-        }
+    if (is_page_template('template-pages/page-nosidebar.php')) {
+        wp_enqueue_style( 'artfolio-layout-style' , get_template_directory_uri() . '/layouts/nosidebar.css');
+    } else {
+        wp_enqueue_style( 'artfolio-layout-style' , get_template_directory_uri() . '/layouts/content-sidebar.css');
+    }
 
-        wp_enqueue_style( 'artfolio-google-fonts', 'https://fonts.googleapis.com/css?family=Merriweather:400,300,400italic,700,900,900italic|Open+Sans:400,400italic,700,700italic' );
+    wp_enqueue_style( 'artfolio-google-fonts', 'https://fonts.googleapis.com/css?family=Merriweather:400,300,400italic,700,900,900italic|Open+Sans:400,400italic,700,700italic' );
 
-        wp_enqueue_style( 'artfolio-fontawesome', 'http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
+    wp_enqueue_style( 'artfolio-fontawesome', 'http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
 
-        wp_enqueue_script( 'artfolio-superfish', get_template_directory_uri() . '/js/superfish.js', array('jquery'), '20160503', true );
+    wp_enqueue_script( 'artfolio-superfish', get_template_directory_uri() . '/js/superfish.js', array('jquery'), '20160503', true );
 
-        wp_enqueue_script( 'artfolio-superfish-configuration', get_template_directory_uri() . '/js/superfish-configuration.js', array('artfolio-superfish'), '20160601', true );
+    wp_enqueue_script( 'artfolio-superfish-configuration', get_template_directory_uri() . '/js/superfish-configuration.js', array('artfolio-superfish'), '20160601', true );
 
-        wp_enqueue_script( 'artfolio-hide-search-panel', get_template_directory_uri() . '/js/hide-search-panel.js', array('jquery'), '20160601', true );
+    wp_enqueue_script( 'artfolio-hide-search-panel', get_template_directory_uri() . '/js/hide-search-panel.js', array('jquery'), '20160601', true );
 
-        wp_enqueue_script( 'artfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    wp_enqueue_script( 'artfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-        wp_enqueue_script( 'artfolio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'artfolio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-        wp_enqueue_script( 'artfolio-masonry', get_template_directory_uri() . '/js/masonry-configuration.js', array('masonry'), '20160601', true );
+    wp_enqueue_script( 'artfolio-masonry', get_template_directory_uri() . '/js/masonry-configuration.js', array('masonry'), '20160601', true );
 
-        if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-            wp_enqueue_script( 'comment-reply' );
-        }
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'artfolio_scripts' );
 

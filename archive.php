9a7ddcd2de165b2d997a7d47cd958f405e2f6235
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-28 10:33:57 +0300

    Added comment

--- archive.php
+++ archive.php
@@ -17,6 +17,7 @@ get_header(); ?>
 
         <header class="page-header">
             <?php
+            // Standart solution for this type of task
             //the_archive_title( '<h1 class="page-title">', '</h1>' );
             //the_archive_description( '<div class="taxonomy-description">', '</div>' );
             ?>

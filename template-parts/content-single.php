99862e4e1983d5bbca12c892321fda60dd5de0cf
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-28 10:40:52 +0300

    #36 Removed title sections from pages

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -14,11 +14,6 @@
     <header class="entry-header">
 
         <?php
-        if ( is_single() ) {
-            the_title( '<h1 class="entry-title">', '</h1>' );
-        } else {
-            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-        }
 
         if ( 'post' === get_post_type() ) : ?>
 

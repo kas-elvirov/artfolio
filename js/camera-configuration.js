cc49b60aaf6871969505c33d2a01bc5069836f4d
Artem Solovev <artem.solovev.lse@gmail.com>
2016-11-01 14:50:54 +0300

    #24 Changed config file for slider

--- js/camera-configuration.js
+++ js/camera-configuration.js
@@ -7,7 +7,7 @@ jQuery(document).ready( function($){
     $('#camera_wrap').camera({
         height: artfolio_IsLargeResolution() ? '500px' : '300px',
         loader: 'bar',
-        pagination: true,
+        pagination: false,
         thumbnails: false,
         time: 4500,
         fx: 'simpleFade'

79131f56614d39a5b8ed769ab8dea4346fe88c4b
Artem Solovev <artem.solovev.lse@gmail.com>
2016-11-01 14:51:50 +0300

    #24 Added slider to INDEX page

--- header.php
+++ header.php
@@ -26,28 +26,40 @@
             <?php if ( !is_404() ) { ?>
 
             <header id="masthead" class="site-header" role="banner">
-                <?php if ( get_header_image() && ( 'blank' == get_header_textcolor() ) ) : ?>
+                <?php if ( is_front_page() && is_home() && get_option( "mainSlider" ) ) { ?>
+
+                <div class="site-branding header-background-image-for-single" style="height:<?php echo esc_attr( get_custom_header()->height ); ?>px">
+
+                    <?php artfolio_display_slider(); ?>
+
+                </div>
+
+                <?php } else { ?>
+
+                <?php if ( get_header_image() && ( 'blank' == get_header_textcolor() ) ) { ?>
+
                 <div class="header-image">
+
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                        <img src="<?php esc_url( header_image() ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+                        <img src="<?php //esc_url( header_image() ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
                     </a>
                 </div>
-                <?php endif; // End header image check. ?>
+                <?php } // End header image check. ?>
 
                 <?php
-                                    /**
-                     * If page has a thumbnail, it is displayed in the header
-                     * */
-                                    if ( ( is_single() || is_page() ) && has_post_thumbnail() ) {
-                                        echo '<div class="site-branding header-background-image-for-single" style="background-image: url(' . 
-                                            get_the_post_thumbnail_url( null, 'large-thumbnails' ) . ');>';
-                                    } else {
-                                        if ( get_header_image() && !( 'blank' == get_header_textcolor() ) ) { 
-                                            echo '<div class="site-branding header-background-image" style="background-image: url(' . get_header_image() . ')">'; 
-                                        } else {
-                                            echo '<div class="site-branding">';
-                                        }
-                                    }
+                             /**
+                * If page has a thumbnail, it is displayed in the header
+                * */
+                             if ( ( is_single() || is_page() ) && has_post_thumbnail() ) {
+                                 echo '<div class="site-branding header-background-image-for-single" style="background-image: url(' . 
+                                     get_the_post_thumbnail_url( null, 'large-thumbnails' ) . ');>';
+                             } else {
+                                 if ( get_header_image() && !( 'blank' == get_header_textcolor() ) ) { 
+                                     echo '<div class="site-branding header-background-image" style="background-image: url(' . get_header_image() . ')">'; 
+                                 } else {
+                                     echo '<div class="site-branding">';
+                                 }
+                             }
                 ?>
 
                 <?php if ( is_single() || is_page() ) { ?> 
@@ -84,6 +96,7 @@
                     </div><!-- .header-box or .header-box-single -->
 
                 </div><!-- .site-branding -->
+                <?php } // end if for SLIDER ?>
 
                 <nav id="site-navigation" class="main-navigation" role="navigation">
                     <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
@@ -102,7 +115,8 @@
                     <div class="search-box clear">
                         <?php get_search_form(); ?>
                     </div>
-                </div> 
+                </div>
+
             </header><!-- #search-container -->
 
             <?php } ?>

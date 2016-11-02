561240f45f156be10df55381d4fe4a9ac0daa610
Artem Solovev <artem.solovev.lse@gmail.com>
2016-11-02 09:35:01 +0300

    Added spider web image for 404 page

--- template-parts/content-404.php
+++ template-parts/content-404.php
@@ -1,5 +1,6 @@
 <div class="error404Title">
     <div class="spider"></div>
+    <div class="spiderWeb"></div>
     <h1 class="statusCode"><?php _e( 'Oops', 'artfolio' ); ?></h1>
     <p><?php _e( 'You seem to be lost. To find what you are looking for try go home or try a search', 'artfolio' ); ?></p>
     <a href="<?php echo get_home_url(); ?>"><i class="fa fa-home"></i></a>

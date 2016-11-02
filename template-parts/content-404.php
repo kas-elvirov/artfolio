9bc17aa651d0039e43a59ae5824b8876df0577f9
Artem Solovev <artem.solovev.lse@gmail.com>
2016-11-02 09:07:36 +0300

    Added another image to 404 page

--- template-parts/content-404.php
+++ template-parts/content-404.php
@@ -1,4 +1,5 @@
 <div class="error404Title">
+    <div class="spider"></div>
     <h1 class="statusCode"><?php _e( 'Oops', 'artfolio' ); ?></h1>
     <p><?php _e( 'You seem to be lost. To find what you are looking for try go home or try a search', 'artfolio' ); ?></p>
     <a href="<?php echo get_home_url(); ?>"><i class="fa fa-home"></i></a>

9c039162e591a4ab1e6f0e33a7745038fede9f5b
Artem Solovev <artem.solovev.lse@gmail.com>
2017-03-16 17:48:49 +0300

    #90 Escaped home_url()

--- template-parts/content-404.php
+++ template-parts/content-404.php
@@ -15,7 +15,7 @@
 
     <p><?php _e( 'You seem to be lost. To find what you are looking for try go home or try a search', 'artfolio' ); ?></p>
 
-    <a href="<?php echo home_url(); ?>">
+    <a href="<?php echo esc_url( home_url() ); ?>">
         <i class="fa fa-home"></i>
     </a>
 
